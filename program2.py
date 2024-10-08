4e2ce62fb2ab273d0a34d1649c9ac856b689dd64
shahilchandra101 <shahilchandra101@gmail.com>
2024-10-08 15:26:35 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
 
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
 
+        for i in range(n):
+
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+
+                total += roman_map[s[i]]
+        
+        return total
+
+sol = Solution()
+print(sol.romanToInt("III"))   
+print(sol.romanToInt("LVIII"))  
+print(sol.romanToInt("MCMXCIV"))
 
