26adbb45bb16b93a6410450d0fd6bb4cf3257c08
shahilchandra101 <shahilchandra101@gmail.com>
2024-10-08 15:55:42 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,10 +1,21 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
-
-
-
+def romanToInt(s: str) -> int:
+    roman_to_int = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    n = len(s)
+    
+    for i in range(n):
+        if i < n - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+            total -= roman_to_int[s[i]]
+        else:
+            total += roman_to_int[s[i]]
+    
+    return total
