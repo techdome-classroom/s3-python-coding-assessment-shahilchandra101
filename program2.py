639b8f3c5baf58223e651ffa32bf41c6a1139886
shahilchandra101 <shahilchandra101@gmail.com>
2024-10-08 15:51:29 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,30 +1,25 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-
-        roman_map = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000
-        }
-        
-        total = 0
-        n = len(s)
-        
-
-        for i in range(n):
-
-            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
-                total -= roman_map[s[i]]
-            else:
-
-                total += roman_map[s[i]]
-        
-        return total
-
-sol = Solution()
-print(sol.romanToInt("III"))   
-print(sol.romanToInt("LVIII"))  
-print(sol.romanToInt("MCMXCIV"))
+def romanToInt(s: str) -> int:
+    # Mapping of Roman numerals to integers
+    roman_to_int = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    n = len(s)
+    
+    # Loop through the string s
+    for i in range(n):
+        # If the current symbol is less than the next one, subtract it
+        if i < n - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+            total -= roman_to_int[s[i]]
+        else:
+            # Otherwise, add it
+            total += roman_to_int[s[i]]
+    
+    return total
