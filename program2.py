425b20b064280bc3021a757b56e7f27c5926ea43
shahilchandra101 <shahilchandra101@gmail.com>
2024-10-08 15:38:49 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,24 +1,30 @@
-def romanToInt(s: str) -> int:
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
 
-    roman_to_int = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    
-    total = 0
-    n = len(s)
-    
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
 
-    for i in range(n):
+        for i in range(n):
 
-        if i < n - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
-            total -= roman_to_int[s[i]]
-        else:
-            total += roman_to_int[s[i]]
-    
-    return total
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+
+                total += roman_map[s[i]]
+        
+        return total
+
+sol = Solution()
+print(sol.romanToInt("III"))   
+print(sol.romanToInt("LVIII"))  
+print(sol.romanToInt("MCMXCIV"))
