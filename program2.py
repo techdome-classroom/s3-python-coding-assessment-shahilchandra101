2f3a3c32572e519e250108f34b66375dbfab03a0
shahilchandra101 <shahilchandra101@gmail.com>
2024-10-08 15:41:21 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,30 +1,56 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
         roman_map = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
         }
         
         total = 0
         n = len(s)
         
-
         for i in range(n):
-
+            
             if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
-                total -= roman_map[s[i]]
+                total -= roman_map[s[i]]  
             else:
-
-                total += roman_map[s[i]]
-        
+                total += roman_map[s[i]]  
+                
         return total
 
-sol = Solution()
-print(sol.romanToInt("III"))   
-print(sol.romanToInt("LVIII"))  
-print(sol.romanToInt("MCMXCIV"))
+
+import unittest
+
+class TestRomanToInt(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example1(self):
+        self.assertEqual(self.solution.romanToInt("III"), 3)
+
+    def test_example2(self):
+        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
+
+    def test_example3(self):
+        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
+
+    def test_single_roman_digit(self):
+        self.assertEqual(self.solution.romanToInt("X"), 10)
+
+    def test_subtraction_rule(self):
+        self.assertEqual(self.solution.romanToInt("IV"), 4)
+        self.assertEqual(self.solution.romanToInt("IX"), 9)
+
+    def test_large_number(self):
+        self.assertEqual(self.solution.romanToInt("MMMCMXCIX"), 3999)
+
+    def test_empty_string(self):
+        self.assertEqual(self.solution.romanToInt(""), 0)
+
+if _name_ == '_main_':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
